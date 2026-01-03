4f8142e5090a6f262fb3df9f89fae9ad24a57595
Yogesh Patil <yogeshpatil432004@gmail.com>
2026-01-03 11:28:44 +0530

    Initial commit

---
+++ python basics.py
@@ -0,0 +1,28 @@
+# revising basic Python concepts
+
+# variables and data types
+name = "Yogesh"
+age = 20
+val = 3.14
+s = True
+print(name)
+print(age)
+print(type(name))
+print(type(age))
+print(type(val))
+print(type(s))
+
+# string functions
+
+text = "Hello, world!"
+
+print(len(text)) 
+print(text.lower())
+print(text.upper())
+print(text.title())  
+print(text.count("o"))   
+print(text.find("lo"))   
+print(text.replace("world", "Python"))
+print(text.split(", "))
+
+# 
